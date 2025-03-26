b1180e66a5120c3f3ef0ed9abcf5c9d1420b05db
kennedy <kennedy.mwai@student.moringaschool.com>
2025-03-26 13:02:04 +0300

    completed quiz

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = [4, 5, 3, 4, 4, 6, 5].reduce((sum, batch) => sum + batch, 0);
+
+module.exports = { totalBatteries };
